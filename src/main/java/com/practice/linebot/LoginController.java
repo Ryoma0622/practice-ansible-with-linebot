6e5db30acf1105f2268ec1d18424f73b20940eb5
上土井涼馬 <sup0ry0jodoi@gmail.com>
2019-06-30 21:00:41 +0900

    url 変更

--- src/main/java/com/practice/linebot/LoginController.java
+++ src/main/java/com/practice/linebot/LoginController.java
@@ -5,29 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
 
-    @GetMapping("/")
-    public String index () {
-        return "redirect:login";
-    }
-
     @GetMapping("/login")
-    public String login () {
+    public String login() {
         return "login";
     }
 
     @GetMapping("/login-success")
-    public String loginSuccess () {
+    public String loginSuccess() {
         return "login-success";
     }
 
     @GetMapping("/login-failure")
-    public String loginFailure () {
+    public String loginFailure() {
         return "login-failure";
     }
 }
